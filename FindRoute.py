bed9b671fe386f44b971c0b7eb5db0765ab01f23
kamlabo <102914217+kamlabo@users.noreply.github.com>
2022-04-16 20:47:00 +0900

    Update FindRoute.py

--- FindRoute.py
+++ FindRoute.py
@@ -1,260 +1,96 @@
-#Choose Value Sort
 import csv
 
-def getPri( x , y , no):
-	f1 = open('csv_file/Priority/Priority'+str(no)+'.csv','r')
-	read = csv.reader( f1 )
-	Pri = [ e for e in read ]
-	f1.close()
-	return float( Pri[ x ][ y ] )
+def getPri(target, no):
+　　f1 = open('CSV/Priority/Priority_' + str(no) + '.csv', 'r')
+　　read = csv.reader(f1)
+　　Pri = [e for e in read]
+　　f1.close()
+　　return float(Pri[target][0])
 
-def getUpPos( a ):
-	if (a != 0):
-		a = a - 1
-	return a
+def chooseNextPoint(now, prev, no, MAX, save):
+　　fvec = open('CSV/Vector.csv', 'r')
+　　Rvec = csv.reader(fvec)
+　　Lvec = [e for e in Rvec]
+　　fvec.close()
+　　
+　　chooseList = []
+　　PriorityList = []
+　　　　for a in range(MAX):
+　　　　　　if(a != prev):
+　　　　　　　　if( Lvec[now][a] == '1' and getPri(a, no) != 0.0):
+　　　　　　　　　　chooseList.append(a)
+　　　　　　
+　　if(len(chooseList) == 1):
+　　　　return chooseList[0]
+　　elif(len(chooseList) >= 2):
+　　　　maxp = chooseList[0]
+　　　　maxpri = getPri(chooseList[0], no)
+　　　　　　
+　　　　for b in range(1, len(chooseList)):
+　　　　　　if(maxpri <= getPri(chooseList[b], no)
+　　　　　　　　　　and savehistory(chooseList[b], save)):
+　　　　　　　　maxp = chooseList[b]
+　　　　　　　　maxpri = getPri(chooseList[b], no)
+　　　　　　	　　
+　　　　return maxp
+　　else:
+　　　　return now
 
-def getDownPos( a, INDEX ):
-	if ( a != ( INDEX - 1 )):
-		a = a + 1
-	return a
+def savehistory(target, save):
+　　if(len(save) <= 1):
+　　　　return 1
+　　else:
+　　　　for a in range(len(save)):
+　　　　　　if(save[a] == target):
+　　　　　　　　return 0
+　　return 1
 
-def getRightPos( b , INDEX):
-	if ( b != INDEX -1 ):
-		b = b + 1
-	return b
+def FindRoute(MAX, no):
+　　#Starting Value
+　　now, prev, purpose, cnt = 0, 0, 21, 1
+　　#Save Point
+　　save = [now]
+　　
+　　while cnt <= 50:
+　　　　now_pri = getPri(now, no)
+　　　　
+　　　　next = chooseNextPoint(now, prev, no, MAX, save)
+　　　　prev = now
+　　　　now = next
+　　　　save.append(now)
+　　　　
+　　　　if(purpose == now):
+　　　　　　break
+　　　　else:
+　　　　　　cnt = cnt + 1
+　　
+　　f2 = open('CSV/FindRoute/FindRoute_' + str(no) + '.csv', 'w')
+　　for a in range(len(save)):
+　　　　f2.write(str(save[a]) + ',')
+　　f2.close()
 
-def getLeftPos( b ):
-	if ( b != 0):
-		b = b - 1
-	return b
+def FindRouteBack(MAX, no):
+　　#Starting Value
+　　now, prev, purpose, cnt = 21, 21, 0, 1
+　　#Save Point
+　　save = [now]
 
-def savehistory(cnt, target_x, target_y, save_x, save_y):
-	if(cnt==1):
-		return 1
-	else:
-		for a in range(cnt):
-			for b in range(cnt):
-				if(save_x[a]==target_x and save_y[b]==target_y):
-					return 0
-	return 1
-	
-def FindRoute( INDEX, no ):
-	
-	#Starting Value
-	now_a, now_b, pre_a, pre_b, cnt = 0, 0, 4, 4, 1
-	#Save Point
-	save_a, save_b = [now_a], [now_b]
+　　while cnt <= 50:
+　　　　now_pri = getPri(now, no)
+　　　　next = chooseNextPoint(now, prev, no, MAX, save)
+　　　　prev = now
+　　　　now = next
+　　　　save.append(now)
+　　　　
+　　　　if(purpose == now):
+　　　　　　break
+　　　　else:
+　　　　　　cnt = cnt + 1
+　　
+　　f2 = open('CSV/FindRoute/FindRoute_' + str(no) + '.csv', 'w')
+　　for a in range(len(save)):
+　　　　f2.write(str(save[a]) + ',')
+　　f2.close()
 
-	while cnt<=50:
-		now_pri = getPri(now_a, now_b, no)
-		print('cnt:'+str(cnt),end='')
-		print('['+str(now_a)+','+str(now_b)+']')
-		#Chooser List
-		up, down, right, left = getUpPos(now_a), getDownPos(now_a,INDEX), getRightPos(now_b,INDEX), getLeftPos(now_b)
-		upp, downp, rightp, leftp = getPri(up, now_b, no), getPri(down, now_b, no), getPri(now_a,right, no), getPri(now_a,left, no)
-		savP = [upp, downp, rightp, leftp]
-		savP.sort()
-		i = 3
-		max=now_pri
-		mark=0
-		while 1:
-			if(savP[i]==1.0):
-				i-=1
-			elif(savP[i]==now_pri):
-				i-=1
-			else:
-				if(savP[i]==rightp and right!=pre_b and now_b!=INDEX-1 and savehistory(cnt,now_a,right,save_a,save_b)):
-					save_a.append(now_a)
-					save_b.append(right)
-					pre_a, pre_b = now_a, now_b
-					now_b = right
-					cnt = cnt + 1
-					break
-				if(savP[i]==downp and down!=pre_a and now_a!=INDEX-1 and savehistory(cnt,down,now_b,save_a,save_b)):
-					save_a.append(down)
-					save_b.append(now_b)
-					pre_a, pre_b = now_a, now_b
-					now_a = down
-					cnt = cnt + 1
-					break
-				if(savP[i]==leftp and left!=pre_b and savehistory(cnt,now_a,left,save_a,save_b)):
-					save_a.append(now_a)
-					save_b.append(left)
-					pre_a, pre_b = now_a, now_b
-					now_b = left
-					cnt = cnt + 1
-					break
-				if(savP[i]==upp and up!=pre_a and savehistory(cnt,up,now_b,save_a,save_b)):
-					save_a.append(up)
-					save_b.append(now_b)
-					pre_a, pre_b = now_a, now_b
-					now_a = up
-					cnt = cnt + 1
-					break
-			i-=1
-			
-			if(i < 0):
-				mark=1
-				break
-			
-			if (((now_a == 4) and (now_b == 4)) or ( cnt > 50 )):
-				mark = 1
-				break
-			
-		if(mark==1):
-			break
-		
-	f2 = open('csv_file/FindRoute/FindRoute'+str(no)+'.csv','w')
-	for a in range(cnt):
-		f2.write(str(save_a[a])+',')
-	f2.write('\n')
-	for a in range(cnt):
-		f2.write(str(save_b[a])+',')
-	f2.close()
-
-def FindRouteBack( INDEX, no ):	
-	#Starting Value
-	now_a, now_b, pre_a, pre_b, cnt = 4, 4, 5, 5, 1
-	#Save Point
-	save_a, save_b = [now_a], [now_b]
-
-	while cnt <= 50:
-		print('cnt:'+str(cnt),end='')
-		print('['+str(now_a)+','+str(now_b)+']')
-		#Chooser List
-		up, down, right, left = getUpPos(now_a), getDownPos(now_a,INDEX), getRightPos(now_b,INDEX), getLeftPos(now_b)
-		upp, downp, rightp, leftp = getPri(up, now_b, no), getPri(down, now_b, no), getPri(now_a,right, no), getPri(now_a,left, no)
-		savP = [upp, downp, rightp, leftp]
-		now_pri = getPri(now_a, now_b, no)
-		savP.sort()
-		i = 3
-		max=now_pri
-		mark=0
-		while 1:
-			if(savP[i]==1.0):
-				i-=1
-			elif(savP[i]==now_pri):
-				i-=1
-			else:
-				if(savP[i]==rightp and right!=pre_b and now_b!=INDEX-1 and savehistory(cnt,now_a,right,save_a,save_b)):
-					save_a.append(now_a)
-					save_b.append(right)
-					pre_a, pre_b = now_a, now_b
-					now_b = right
-					cnt = cnt + 1
-					break
-				if(savP[i]==downp and down!=pre_a and now_a!=INDEX-1 and savehistory(cnt,down,now_b,save_a,save_b)):
-					save_a.append(down)
-					save_b.append(now_b)
-					pre_a, pre_b = now_a, now_b
-					now_a = down
-					cnt = cnt + 1
-					break
-				if(savP[i]==leftp and left!=pre_b and savehistory(cnt,now_a,left,save_a,save_b)):
-					save_a.append(now_a)
-					save_b.append(left)
-					pre_a, pre_b = now_a, now_b
-					now_b = left
-					cnt = cnt + 1
-					break
-				if(savP[i]==upp and up!=pre_a and savehistory(cnt,up,now_b,save_a,save_b)):
-					save_a.append(up)
-					save_b.append(now_b)
-					pre_a, pre_b = now_a, now_b
-					now_a = up
-					cnt = cnt + 1
-					break
-				i = i - 1
-			if(i < 0):
-				mark=1
-				break
-			
-			if (((now_a == 0) and (now_b == 0)) or ( cnt > 50 )):
-				mark = 1
-				break
-		if(mark==1):
-			break
-	
-	f2 = open('csv_file/FindRoute/FindRoute'+str(no)+'.csv','w')
-	for a in range(cnt):
-		f2.write(str(save_a[a])+',')
-	f2.write('\n')
-	for a in range(cnt):
-		f2.write(str(save_b[a])+',')
-	f2.close()
-	
-def FindRouteBack2( INDEX, no ):	
-	#Starting Value
-	now_a, now_b, pre_a, pre_b, cnt = 4, 4, 5, 5, 1
-	now_pri = getPri(now_a, now_b)
-	#Save Point
-	save_a, save_b, save_pri = [now_a], [now_b], [now_pri]
-
-	while cnt <= 50:
-		print('cnt:'+str(cnt),end='')
-		print('['+str(now_a)+','+str(now_b)+']')
-		#Chooser List
-		up, down, right, left = getUpPos(now_a), getDownPos(now_a,INDEX), getRightPos(now_b,INDEX), getLeftPos(now_b)
-		upp, downp, rightp, leftp = getPri(up, now_b), getPri(down, now_b), getPri(now_a,right), getPri(now_a,left)
-		savP = [upp, downp, rightp, leftp]
-		savP.sort()
-		i = 3
-		max=now_pri
-		mark=0
-		while 1:
-			if(savP[i]==rightp and right!=pre_b and now_b!=INDEX-1 and savehistory(cnt,now_a,right,save_a,save_b)):
-				save_pri.append(rightp)
-				save_a.append(now_a)
-				save_b.append(right)
-				pre_a, pre_b = now_a, now_b
-				now_b = right
-				cnt = cnt + 1
-				break
-			if(savP[i]==downp and down!=pre_a and now_a!=INDEX-1 and savehistory(cnt,down,now_b,save_a,save_b)):
-				save_pri.append(downp)
-				save_a.append(down)
-				save_b.append(now_b)
-				pre_a, pre_b = now_a, now_b
-				now_a = down
-				cnt = cnt + 1
-				break
-			if(savP[i]==leftp and left!=pre_b and savehistory(cnt,now_a,left,save_a,save_b)):
-				save_pri.append(leftp)
-				save_a.append(now_a)
-				save_b.append(left)
-				pre_a, pre_b = now_a, now_b
-				now_b = left
-				cnt = cnt + 1
-				break
-			if(savP[i]==upp and up!=pre_a and savehistory(cnt,up,now_b,save_a,save_b)):
-				save_pri.append(upp)
-				save_a.append(up)
-				save_b.append(now_b)
-				pre_a, pre_b = now_a, now_b
-				now_a = up
-				cnt = cnt + 1
-				break
-			i = i - 1
-			if(i < 0):
-				mark=1
-				break
-			
-			if (((now_a == 0) and (now_b == 0)) or ( cnt > 50 )):
-				mark = 1
-				break
-		if(mark==1):
-			break
-	
-	f2 = open('csv_file/FindRoute/FindRoute'+str(no)+'.csv','w')
-	for a in range(cnt):
-		f2.write(str(save_a[a])+',')
-	f2.write('\n')
-	for a in range(cnt):
-		f2.write(str(save_b[a])+',')
-	f2.close()
-	
-if __name__=='__main__':
-	no = 5
-	FindRoute(5,no)
-	#FindRouteBack(5,no)
-	#FindRouteBack2(5,3)
+if __name__ == '__main__':
+　　pass
