b83e3ff73603af0ad1a992f88e1de88152886374
kamlabo <102914217+kamlabo@users.noreply.github.com>
2022-04-16 20:46:03 +0900

    Update MakePri.py

--- MakePri.py
+++ MakePri.py
@@ -1,54 +1,69 @@
 import math
 import csv
 
-def MakePri(INDEX,pd,speed,no):
+def DistCal(Lati, Long, Lati0, Long0, LatiEnd, LongEnd):
+　　SET_A = math.sqrt(pow((Lati0 - Lati) / 0.0111, 2)
+　　　　　　　　　　+ pow((Long0 - Long) / 0.0091, 2))
+　　SET_B = math.sqrt(pow((LatiEnd - Lati) / 0.0111, 2)
+　　　　　　　　　　+ pow((LongEnd - Long) / 0.0091, 2))
+　　if(SET_A < SET_B):
+　　　　return SET_A
+　　return SET_B
 
-	al, be, g, d = 0.01*pd, 0.001, 0.99, 0.1	#The constant value
-	
-	f1 = open('csv_file/Danger_v2.csv')
-	read = csv.reader(f1)
-	dang = [ e for e in read ]
-	f1.close()
-	
-	f2 = open('csv_file/User/User'+str(no)+'.csv')
-	read = csv.reader(f2)
-	User = [ e for e in read ]
-	f2.close()
-	
-	f3 = open('csv_file/Distance_v2.csv')
-	read = csv.reader(f3)
-	Dist = [ e for e in read ]
-	f3.close()
-	
-	f4 = open('csv_file/Priority/Priority'+str(no)+'.csv','w')
-	
-	for a in range(INDEX):
-		for b in range(INDEX):
-			D = dang[a][b]
-			Us = User[a][b]
-			Dp = Dist[a][b]
-			BlockA = math.floor(float(Dp)*pd)*al
-			BlockB = be*BlockA/speed/60
-			BlockC = g*float(Us)
-			BlockD = d*float(D)
-			if(float(Us)==0):
-				priority = 0.000
-			elif(float(D)==1):
-				priority = 1.000
-			else:
-				priority = math.floor((BlockA+BlockB+BlockC+BlockD)*1000)/1000
-				if(priority>1.000):
-					priority=1.000
-			print("["+str(a)+"]["+str(b)+"] Dp:"+str(math.floor(float(Dp)*pd*100)/100)+"\tD:"+D+"\tUs:"+Us+"\tPri:"+str(priority))
-			f4.write(str(priority))
-			if(b==(INDEX-1)):
-				break
-			else:
-				f4.write(",")
-		f4.write("\n")
-	f4.close()
+def MakePri2(MAX, speed, no):
+　　f1 = open('CSV/Danger/Danger_' + str(no) + '.csv')
+　　read = csv.reader(f1)
+　　dang = [e for e in read]
+　　f1.close()
+　　
+　　f2 = open('CSV/User/User_' + str(no) + '.csv')
+　　read = csv.reader(f2)
+　　User = [e for e in read]
+　　f2.close()
+　　
+　　f3 = open('CSV/Map.csv')
+　　read = csv.reader(f3)
+　　Dist = [e for e in read]
+　　f3.close()
+　　
+　　pd = math.sqrt(
+　　　　　　　　pow((float(Dist[0][1]) - float(Dist[MAX - 1][1])) / 0.0111, 2)+
+　　　　　　　　pow((float(Dist[0][2]) - float(Dist[MAX - 1][2])) / 0.0091, 2))
+　　
+　　#The constant value
+　　al, be, g, d = 0.0001 * pd, 0.001, 0.99, 0.1
+　　f4 = open('CSV/Priority/Priority_' + str(no) + '.csv', 'w')
+　　
+　　for a in range(MAX):
+　　　　D = dang[a][1]
+　　　　Us = User[a][1]
+　　　　Dp = DistCal(float(Dist[a][1]), float(Dist[a][2]), 
+　　　　　　　　　　float(Dist[0][1]), float(Dist[0][2]),
+　　　　　　　　　　float(Dist[MAX - 1][1]), float(Dist[MAX - 1][2]))
+　　　　
+　　　　Bl_A = math.floor(float(Dp)) * al
+　　　　Bl_B = be * BlockA / speed / 60
+　　　　Bl_C = g * float(Us)
+　　　　Bl_D = d * float(D)
+　　　　
+　　　　if(float(Us) == 0):
+　　　　　　priority = 0.000
+　　　　elif(float(D) == 1 or a == 0 or a == MAX - 1):
+　　　　　　priority = 1.000
+　　　　else:
+　　　　　　priority = math.floor((Bl_A + Bl_B + Bl_C + Bl_D) * 1000) / 1000
+　　　　　　if(priority > 1.000):
+　　　　　　　　priority = 1.000
+　　　　
+　　　　f4.write(str(priority))
+　　　　f4.write('\n')
+　　f4.close()
+　　
+　　#ParamSaves
+　　f5 = open('CSV/Priority/PriParams.csv', 'a')
+　　f5.write(str(no) + ',' + str(al) + ',' + str(be) + ',' + str(g) + ','
+　　　　　　　　+ str(d) + '\n') 
+　　f5.close()
 
 if __name__=='__main__':
-	#pass
-	#INDEX,pd,speed,no
-	MakePri(5,5,4,5)
+　　pass
